544ff1fa8a7640d3c323f2f8d7f528314ca220cb
oxiblaze <altatharvaa150@gmail.com>
2026-06-16 22:14:58 +0530

    codecrafters submit [skip ci]

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input + ": command not found");
+    }
+}
