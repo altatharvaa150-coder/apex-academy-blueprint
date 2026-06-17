12181f7058ecd40b324f7f1407d5d780ae7481eb
oxiblaze <altatharvaa150@gmail.com>
2026-06-17 14:51:39 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-            if (input.equals("exit")) {
-                break;
-            } else if (input.startsWith("echo ")) {
-                System.out.println(input.substring(5));
-            } else {
-                System.out.println(input + ": command not found");
+            if (input.equals("exit")|| input.equals("quit")||input.equals("type")) {
+                System.out.println("is a shell button");
             }
+            System.out.println(input + ": command not found");
         }
     }
 }
