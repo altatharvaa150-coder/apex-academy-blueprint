df36c07e19937465d8f91e23b404ee7cc48a60d8
oxiblaze <altatharvaa150@gmail.com>
2026-06-17 14:32:17 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
- System.out.print("$ ");
-            String input = Scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
             if (input.equals("exit")) {
                 break;
             }
             System.out.println(input + ": command not found");
         }
-    }       
-    
+    }
+}
